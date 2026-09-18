78300cc8bbbebcc8dfe3043f924969281979650e
AngelikaKulbacka <angela.1717_99@o2.pl>
2026-09-18 13:10:28 +0200

    Initial commit: CheersChecklist project

---
+++ shared/src/jvmMain/kotlin/com/example/cheerschecklist/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.example.cheerschecklist
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
